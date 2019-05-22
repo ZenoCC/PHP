4a28a9f0c81d451520dc2015cac05d67deda8e30
zeno <814383122@qq.com>
2019-05-22 15:59:16 +0800

    pull from remote server

--- index.php
+++ index.php
@@ -7,11 +7,8 @@ echo "add sth in github editor";
 #clone 后的编辑
 echo "i build a repository on 5/22!"
 $a =5;
-<<<<<<< HEAD
 echo $a;
 echo "in a new branch devzeno";
-=======
 #echo $a;
 #本地push后在线编辑（模拟多用户上传操作）
 echo 'HHHH';
->>>>>>> 1daa5f055828b08bb158b069cc1360b06e9179fd
