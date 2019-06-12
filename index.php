8f8966df7c2a34efcdf3e93164b3883dfea68d95
Na Na3 Shao <shaona3@Lenovo.com>
2019-06-12 17:42:17 +0800

    modify index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo "git linked in github";
 echo "add sth in github editor";
 #clone 后的编辑
 echo "i build a repository on 5/22!"
-$a =5;
+$a =6;
 echo $a;
 echo "in a new branch devzeno";
 #echo $a;
